29d80ab5c1a7451542c795de05ad5a1d73a8376b
pawanbhatt1213 <pawanbhatt1213@gmail.com>
2023-06-26 18:25:44 +0530

    routes and component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MovieApilService } from 'src/app/service/movie-apil.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+
+  bannerData: any;
+
+  constructor(private movieApi: MovieApilService){}
+
+  ngOnInit(){
+    this.movieApi.bannerApiData().subscribe((res)=>this.bannerData=res.results)
+  }
+
+
+}
